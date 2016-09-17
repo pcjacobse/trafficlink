19df49c19ac546d4ef8009918802afeac7453608
Paul Jacobse <pcjacobse@gmail.com>
2016-09-18 00:27:02 +0200

    Traject data in rest action Query is still slow, maybe docker ?

--- code/src/App/Action/RestAction.php
+++ code/src/App/Action/RestAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Action;
 
+use App\Domain\Traject\Traject;
 use Doctrine\ORM\EntityManager;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -18,7 +19,35 @@ class RestAction
 
     public function __invoke(ServerRequestInterface $request, ResponseInterface $response, callable $next = null)
     {
+        /** @var Traject[] $trajects */
+        $trajects = $this->entityManager->getRepository('App\Domain\Traject\Traject')->findAllWithStatusFromLastHour();
+
         $data = [];
+        foreach ($trajects as $traject) {
+            $geometry = [];
+            foreach ($traject->getGeometryPoints() as $geometryPoint) {
+                $geometry[] = [
+                    'latitude'  => $geometryPoint->getLatitude(),
+                    'longitude' => $geometryPoint->getLongitude(),
+                ];
+            }
+
+            $statuses = [];
+            foreach ($traject->getStatuses() as $status) {
+                $statuses[] = [
+                    'measuredAt' => $status->getMeasuredAt()->format('Y-m-d\TH:i:s'),
+                    'velocity'   => $status->getVelocity(),
+                    'traveltime' => $status->getTraveltime(),
+                ];
+            }
+
+            $data[] = [
+                'name'     => $traject->getName(),
+                'geometry' => $geometry,
+                'statuses' => $statuses,
+            ];
+        }
+
         return new JsonResponse($data);
     }
 }
--- code/src/App/Domain/Traject/TrajectRepository.php
+++ code/src/App/Domain/Traject/TrajectRepository.php
@@ -15,4 +15,23 @@ class TrajectRepository extends EntityRepository
     {
         return $this->find([], ['name' => 'ASC']);
     }
+
+    /**
+     * Get all trajects and statuses from the last hour
+     *
+     * @return Traject[]
+     */
+    public function findAllWithStatusFromLastHour()
+    {
+        $time = new \DateTime();
+        $time->sub(new \DateInterval('PT1H'));
+
+        return $this->createQueryBuilder('t')
+            ->innerJoin('t.statuses', 's')
+            ->addSelect('s')
+            ->where('s.measuredAt > :time')
+            ->setParameter('time', $time)
+            ->getQuery()
+            ->getResult();
+    }
 }
