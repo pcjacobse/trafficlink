398c8d0c18c1b54d3b0d5923fdcc5836daecbe15
Paul Jacobse <pcjacobse@gmail.com>
2016-09-18 00:27:02 +0200

    Run migrations on launch

--- php7-fpm/setup.sh
+++ php7-fpm/setup.sh
@@ -5,7 +5,7 @@ pushd /code
 rm -Rf /code/{vendor,composer.lock,composer.phar}
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
-
+php vendor/bin/doctrine-migrations migrations:migrate -n
 popd
 
 php-fpm
